eaa47eb05c7721db4a1ad079570a61cd2b634870
jamesgayflor <shekep1000@gmail.com>
2024-10-17 10:08:27 +0000

    Updates

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const serverless = require("serverless-http");
+const router = express.Router();
+const port = 3400;
+
+// Static files render configurations
+app.use('/public', express.static(__dirname + '/public/'));
+
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+// Get Routes
+
+app.get('/', (req, res) => {
+    res.render('aws_dashboard');
+});
+
+
+// Post Routes
+
+
+// Hosting Configuration
+
+// app.use("/.netlify/functions/app", router);
+// module.exports.handler = serverless(app);
+
+app.listen(port, () => {
+    console.log(`Server is live on: ${port}`);
+})
